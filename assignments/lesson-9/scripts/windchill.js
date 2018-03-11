b0da0ce227efdb7dd982be165f9de313be8fc2c7
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-03-10 19:12:24 -0700

    lesson 9 more updates

--- assignments/lesson-9/scripts/windchill.js
+++ assignments/lesson-9/scripts/windchill.js
@@ -2,4 +2,6 @@ var a = (90 + 66) / 2;
 
 var f = 35.74 + .6215 * a - 35.75 * Math.pow(5, .16) + .4275 * a * Math.pow(5, .16);
 
-document.getElementById("windchill").innerHTML = (f.toFixed(0));
+document.getElementById("windchill").innerHTML = (f.toFixed(0)); 
+
+//formula: f = 35.74 + 0.6215 t - 35.75 s^0.16 + 0.4275 t s^0.16
---
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -0,0 +1,19 @@
+var requestURL = 'http://api.wunderground.com/api/997fa104ce7cd04a/conditions/forecast/q/MN/Franklin.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
+
+request.onload = function() {
+    var franklinWeather = JSON.parse(request.responseText);
+    //console.log(franklinWeather);
+    
+    document.getElementById('weatherCondition').innerHTML = franklinWeather.current_observation.weather;
+    
+    document.getElementById('temp').innerHTML = franklinWeather.current_observation.temp_f;
+    
+    document.getElementById('weather_icon').src = franklinWeather.current_observation.icon_url;
+    
+    document.getElementById('windspeed').innerHTML = franklinWeather.current_observation.wind_mph;
+    
+    document.getElementById('txtForecast').innerHTML = franklinWeather.forecast.txt_forecast.forecastday["0"];
+}
