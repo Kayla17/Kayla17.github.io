08828269232391224546c3dd0b91d13b868c4b25
Kayla17 <35283831+Kayla17@users.noreply.github.com>
2018-03-16 23:49:35 -0600

    L09 placeholder and wuJS

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,17 +1,28 @@
-var requestURL = 'http://api.wunderground.com/api/997fa104ce7cd04a/conditions/forecast/q/MN/Franklin.json';
+var requestURL = '//api.wunderground.com/api/997fa104ce7cd04a/conditions/forecast/q/MN/Franklin.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL, true);
 request.send();
 
 request.onload = function() {
     var franklinWeather = JSON.parse(request.responseText);
-    //console.log(franklinWeather);
+    console.log(franklinWeather);
     
     document.getElementById('weatherCondition').innerHTML = franklinWeather.current_observation.weather;
     
     document.getElementById('temp').innerHTML = franklinWeather.current_observation.temp_f;
     
-    document.getElementById('weather_icon').src = franklinWeather.current_observation.icon_url;
+    var icon_path = franklinWeather.current_observation.icon_url;
+    
+    var urlString = document.location.href;
+    console.log(urlString);
+    var found = urlString.indexOf("https");
+    console.log(found);
+    
+    if(found>=0) {
+        icon_path = icon_path.replace("http","https");
+    }
+    
+    document.getElementById('weather_icon').src = icon_path
     
     document.getElementById('windspeed').innerHTML = franklinWeather.current_observation.wind_mph;
     
